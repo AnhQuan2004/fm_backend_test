d25bdc78e729dd496a604eafd526113470072554
AnhQuan2004 <anhquan200304@gmail.com>
2025-11-15 11:21:21 +0700

    add cors for cor

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -12,22 +12,7 @@ export function middleware(req: NextRequest) {
       return NextResponse.json({ ok: false, error: "Unauthorized" }, { status: 401 });
     }
   }
-  const res = NextResponse.next();
-
-  // Add CORS headers
-  res.headers.set("Access-Control-Allow-Origin", "https://first-movers.vercel.app");
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return new NextResponse(null, { status: 204, headers: res.headers });
-  }
-
-  return res;
+  return NextResponse.next();
 }
 
 export const config = {
